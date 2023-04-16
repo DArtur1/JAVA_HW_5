147369c9bc8e45721dee7ad693bc8d0a3a882f47
DudkaAV_QA <arturdudka@yandex.ru>
2023-04-16 20:21:56 +0700

    Parameterized Test

--- src/test/java/ru/netology/services/employeeVacationTest.java
+++ src/test/java/ru/netology/services/employeeVacationTest.java
@@ -1,12 +1,15 @@
 package ru.netology.services;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 public class employeeVacationTest {
-    @Test
-    public void testVocantion(){
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/employeeBenefits.csv")
+    public void testVocantionLimit(int income, int expenses, int threshold){
         employeeVacation relax = new employeeVacation();
 
-        int count = relax.calculate(10000, 3000, 20000);
+        int count = relax.calculate(income,expenses, threshold);
         System.out.println("Можно отодохнуть " + count + " месяца(ев)");
     }
 
